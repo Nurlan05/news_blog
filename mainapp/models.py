ed1a6da78fc4e079a93ac497c84f035456ec820d
Your Name <you@example.com>
2022-07-10 20:15:06 +0400

    data

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class News(models.Model):
-    title=models.CharField(max_length=1500,verbose_name="News title")
-    content=models.TextField(verbose_name="News content")
+    title=models.CharField(max_length=1500,verbose_name="News title",blank=True)
+    content=models.TextField(verbose_name="News content",blank=True)
     image=models.FileField(verbose_name="News image",blank=True)
     date=models.DateField(verbose_name="Tarix",null=True,blank=True)
     draft=models.BooleanField(default=True,verbose_name="is Publish?")
