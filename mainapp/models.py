b6ef821504c4cba00ab23481be8ee38ae5951d67
Your Name <you@example.com>
2022-07-10 15:52:23 +0400

    first commit

---
+++ mainapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class News(models.Model):
+    title=models.CharField(max_length=1500,verbose_name="News title")
+    content=models.TextField(verbose_name="News content")
+    image=models.FileField(verbose_name="News image")
+    date=models.DateField(verbose_name="Tarix",null=True)
+    draft=models.BooleanField(default=True,verbose_name="is Publish?")
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name="News"
+        verbose_name_plural="News"
+        ordering=['id']
+
+
+
+
---
+++ mainapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from mainapp.models import News
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import NewsSerializers
+
+
+@api_view(['GET'])
+def index(request):
+    news = News.objects.all()
+    serializer = NewsSerializers(news, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def newsDetail(request, pk):
+    news = News.objects.get(id=pk)
+    serializer = NewsSerializers(news, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def newsCreate(request):
+    serializer = NewsSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def newsUpdate(request, pk):
+    news = News.objects.get(id=pk)
+    serializer = NewsSerializers(instance=news, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def newsDelete(request, pk):
+    news = News.objects.get(id=pk)
+    news.delete()
+    return Response("Successfully deleted!")
